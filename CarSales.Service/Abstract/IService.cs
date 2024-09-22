3bf487d09b8afd4e8543aedf148d42dcb884d01e
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-09-22 22:59:54 +0300

    Service updated

--- CarSales.Service/Abstract/IService.cs
+++ CarSales.Service/Abstract/IService.cs
@@ -1,14 +1,9 @@
 ﻿using CarSales.Data.Abstract;
 using CarSales.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CarSales.Service.Abstract
 {
-    public interface IService<T> : IGenericRepository<T, int> where T : class, IEntity<int>, new()
+    public interface IService<T, TDbContext> : IGenericRepository<T, int> where T : class, IEntity<int>, new()
     {
     }
 }
--- CarSales.Service/Concrete/Service.cs
+++ CarSales.Service/Concrete/Service.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CarSales.Service.Concrete
 {
-    public class Service<T, TDbContext> : GenericRepository<T, TDbContext, int>, IService<T> where T : class, IEntity<int>, new() where TDbContext : DbContext
+    public class Service<T, TDbContext> : GenericRepository<T, TDbContext, int>, IService<T, TDbContext> where T : class, IEntity<int>, new() where TDbContext : DbContext
     {
         public Service(TDbContext dbContext) : base(dbContext)
         {
