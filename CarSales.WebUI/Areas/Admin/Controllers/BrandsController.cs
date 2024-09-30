64b38a5636eedf1048e69b437a03e9a248dfcd8e
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-09-30 20:06:50 +0300

    Brands Controller created

---
+++ CarSales.WebUI/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,87 @@
+﻿using CarSales.Data;
+using CarSales.Entities;
+using CarSales.Service.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandsController : Controller
+    {
+        private readonly IService<Brand, CarDbContext> _service;
+
+        public BrandsController(IService<Brand, CarDbContext> service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync(Brand brand)
+        {
+            try
+            {
+                await _service.InsertOneAsync(brand);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Hata Oluştu");
+            }
+            return View(brand);
+        }
+        public async Task<IActionResult> EditAsync(int id)
+        {
+            await _service.GetByIdAsync(id);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Brand brand)
+        {
+            try
+            {
+                _service.UpdateOne(brand);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Hata Oluştu");
+            }
+            return View(brand);
+        }
+
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Brand brand)
+        {
+            try
+            {
+                _service.DeleteOne(brand);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
