0aa6f412fc40e808ad946f2e97dc6c511cbe6d8d
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-09-30 19:45:09 +0300

    migration updated

---
+++ CarSales.Data/Migrations/20240930163148_DataAnnotionsAdded.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarSales.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataAnnotionsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UserCreateDate",
+                value: new DateTime(2024, 9, 30, 19, 31, 48, 258, DateTimeKind.Local).AddTicks(3793));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UserCreateDate",
+                value: new DateTime(2024, 9, 22, 16, 2, 0, 529, DateTimeKind.Local).AddTicks(44));
+        }
+    }
+}
