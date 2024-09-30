ab9bff644a682e55244fe3c5702a6de32d43e6df
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-09-30 19:45:55 +0300

    RolesController created

---
+++ CarSales.WebUI/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,83 @@
+﻿using CarSales.Data;
+using CarSales.Entities;
+using CarSales.Service.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarSales.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly IService<Role, CarDbContext> _service;
+
+        public RolesController(IService<Role, CarDbContext> service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync(Role role)
+        {
+            try
+            {
+                await _service.InsertOneAsync(role);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public async Task<IActionResult> EditAsync(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Role role)
+        {
+            try
+            {
+                _service.UpdateOne(role);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Role role)
+        {
+            try
+            {
+                _service.DeleteOne(role);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
