73a878f30a6fb9ca0d9eaf0fc6e94c5b90ebd58a
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-09-30 19:46:06 +0300

    UserController created

---
+++ CarSales.WebUI/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,102 @@
+﻿using CarSales.Data;
+using CarSales.Entities;
+using CarSales.Service.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CarSales.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly IService<User, CarDbContext> _service;
+        private readonly IService<Role, CarDbContext> _serviceRole;
+
+
+        public UsersController(IService<User, CarDbContext> service, IService<Role, CarDbContext> serviceRole)
+        {
+            _service = service;
+            _serviceRole = serviceRole;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        public async Task<IActionResult> CreateAsync()
+        {
+            ViewBag.RoleId = new SelectList(await _serviceRole.GetAllAsync(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    await _service.InsertOneAsync(user);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata oluştu");
+                }
+            }
+            ViewBag.RoleId = new SelectList(await _serviceRole.GetAllAsync(), "Id", "Name");
+            return View(user);
+
+        }
+
+        public async Task<IActionResult> EditAsync(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            ViewBag.RoleId = new SelectList(await _serviceRole.GetAllAsync(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditAsync(int id, User user)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _service.UpdateOne(user);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata oluştu");
+                }
+            }
+            ViewBag.RoleId = new SelectList(await _serviceRole.GetAllAsync(), "Id", "Name");
+            return View(user);
+        }
+
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var model = await _service.GetByIdAsync(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(User user)
+        {
+            try
+            {
+                _service.DeleteOne(user);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
