f321eb49cf5f467f94efed40ddc7391cf385a67c
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-10-07 02:36:14 +0300

    UnitOfWorkCreated

---
+++ CarSales.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CarSales.Data.Abstract
+{
+    public interface IUnitOfWork<TDbContext>
+    {
+        Task BeginTransactionAsync(CancellationToken cancellationToken = default(CancellationToken));
+        void BeginTransaction();
+        Task CommitAsync(CancellationToken cancellationToken = default(CancellationToken));
+        void Commit();
+        Task RollbackAsync(CancellationToken cancellationToken = default(CancellationToken));
+        void Rollback();
+    }
+}
