c5088d41c45473978e953edd2aa5eebe472eea0d
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-10-16 01:48:27 +0300

    CarService created

---
+++ CarSales.Service/Concrete/CarService.cs
@@ -0,0 +1,13 @@
+﻿using CarSales.Data;
+using CarSales.Data.Concrete;
+using CarSales.Service.Abstract;
+
+namespace CarSales.Service.Concrete
+{
+    public class CarService : CarRepository<CarDbContext>, ICarService
+    {
+        public CarService(CarDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
