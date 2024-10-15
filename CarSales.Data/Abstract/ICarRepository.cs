70d3ff3ee853912251cc4efaf738846e5137a536
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-10-16 01:48:10 +0300

    CarRepository updated

--- CarSales.Data/Abstract/ICarRepository.cs
+++ CarSales.Data/Abstract/ICarRepository.cs
@@ -1,8 +1,12 @@
 ﻿using CarSales.Entities;
+using System.Linq.Expressions;
 
 namespace CarSales.Data.Abstract
 {
     public interface ICarRepository : IGenericRepository<Car, int>
     {
+        Task<Car?> CarwithBrandFirstOrDefaultAsync (int id, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Car?>> CarwithBrandListAsync(Expression<Func<Car, bool>> selector, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Car?>> CarwithBrandListAsync (CancellationToken cancellationToken = default);
     }
 }
--- CarSales.Data/Concrete/CarRepository.cs
+++ CarSales.Data/Concrete/CarRepository.cs
@@ -1,13 +1,29 @@
 ﻿using CarSales.Data.Abstract;
 using CarSales.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace CarSales.Data.Concrete
 {
-    public class CarRepository<TDbContext> : GenericRepository<Car, TDbContext, int>, ICarRepository where TDbContext : DbContext
+    public class CarRepository<TDbContext> : GenericRepository<Car, CarDbContext, int>, ICarRepository
     {
-        public CarRepository(TDbContext dbContext) : base(dbContext)
+        public CarRepository(CarDbContext dbContext) : base(dbContext)
         {
+        }
+
+        public async Task<Car?> CarwithBrandFirstOrDefaultAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Cars.Include(x => x.Brand).FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
+
+        public async Task<IEnumerable<Car?>> CarwithBrandListAsync(Expression<Func<Car, bool>> selector, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Cars.Include(x => x.Brand).Where(selector).ToListAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<Car?>> CarwithBrandListAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Cars.Include(x => x.Brand).ToListAsync(cancellationToken);
         }
     }
 }
