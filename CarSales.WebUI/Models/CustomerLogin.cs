3d2744db81f7d3a73f59d655ee928f38c507905a
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2024-10-16 01:50:22 +0300

    Models created

---
+++ CarSales.WebUI/Models/CustomerLogin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSales.WebUI.Models
+{
+    public class CustomerLogin
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ CarSales.WebUI/Models/HomePageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CarSales.Entities;
+
+namespace CarSales.WebUI.Models
+{
+    public class HomePageViewModel
+    {
+        public IEnumerable<Slider> Sliders { get; set; }
+        public IEnumerable<Car> Cars { get; set; }
+
+    }
+}
